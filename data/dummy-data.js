b297c0d0f39407564cc2bcc715eb2a7e402666cf
Mira Manger <mira.manger@haufe-lexware.com>
2021-06-18 22:14:27 +0200

    added dummy data

---
+++ data/dummy-data.js
@@ -0,0 +1,85 @@
+import Artwork from '../models/artwork';
+
+export const ARTWORK_LIST = [
+  new Artwork(
+    1,
+    'Ohne Titel (Skulptur für Freiburg)',
+    ['Rückriem, Ulrich'],
+    '1986',
+    'Granit, in fünf Teile gespalten und in der ursprünglichen Form wieder zusammengesetzt',
+    '568 x 108 x 137 cm',
+    'Augustinerplatz',
+    1,
+    1,
+    ['https://de.wikipedia.org/wiki/Ulrich_Rückriem'],
+    'https://res.cloudinary.com/julidarockt/image/upload/v1624046770/Ru%CC%88ckriem_Augustinerplatz.jpg'
+  ),
+  new Artwork(
+    2,
+    'Tanzende Kegel',
+    ['Zaumseil, Andrea'],
+    '1996/2000',
+    'Eisenguss',
+    'Höhen zwischen 300 und 340 cm; Neigungswinkel 60, 70, 75 und 80 Grad',
+    'Konzerthaus',
+    1,
+    1,
+    ['https://de.wikipedia.org/wiki/Andrea_Zaumseil'],
+    'https://res.cloudinary.com/julidarockt/image/upload/v1624046776/Zaumseil_Tanzende_Kegel.jpg'
+  ),
+  new Artwork(
+    3,
+    'Ohne Titel',
+    ['Hüppi, Thaddäus'],
+    '2004',
+    'Edelstahl, Bronze',
+    '460 cm',
+    'Wiehre',
+    1,
+    1,
+    ['https://de.wikipedia.org/wiki/Thaddäus_Hüppi'],
+    'https://res.cloudinary.com/julidarockt/image/upload/v1624046772/Hu%CC%88ppi_Bu%CC%88ste_auf_Sa%CC%88ule_Wiehre_I.jpg'
+  ),
+  new Artwork(
+    4,
+    'Badisches Saukalb',
+    ['Hohorst, Astrid'],
+    '2009',
+    'Epoxidharz auf Betonsockel, teilweise rot lackiert',
+    'Kalb: 90 x 125 x 140 cm; Sockel: 120 x 75 x 160 cm',
+    'Wiehre',
+    1,
+    1,
+    [],
+    'https://res.cloudinary.com/julidarockt/image/upload/v1624046772/Hohorst_Badisches_Saugkalb.jpg'
+  ),
+  new Artwork(
+    5,
+    'Gartenschlauch',
+    ['Oldenburg, Claes', 'van Bruggen, Coosje'],
+    '1983',
+    'Stahl',
+    '10 m hoch, 84 langer roter Schlauch',
+    'Escholzpark',
+    1,
+    1,
+    [
+      'https://de.wikipedia.org/wiki/Claes_Oldenburg',
+      'https://fudder.de/kunst-die-rumsteht-1-claes-oldenburgs-wasserschlauch--118361228.html',
+    ],
+    'https://res.cloudinary.com/julidarockt/image/upload/v1624046772/Oldenburg_Gartenschlauch.jpg'
+  ),
+  new Artwork(
+    6,
+    'Heroischer Rhythmus IX',
+    ['Lardera, Berto'],
+    '1969',
+    'Stahl',
+    '',
+    'Bahnhof',
+    1,
+    1,
+    ['https://de.wikipedia.org/wiki/Berto_Lardera'],
+    'https://res.cloudinary.com/julidarockt/image/upload/v1624046775/Ladera_Heroischer_Rythmus.jpg'
+  ),
+];
---
+++ models/artwork.js
@@ -0,0 +1,17 @@
+class Artwork {
+  constructor(id, title, artists, year, material, dimensions, location, longitude, latitude, infoUrls, pictureUrl) {
+    this.id = id;
+    this.title = title;
+    this.artists = artists;
+    this.year = year;
+    this.material = material;
+    this.dimensions = dimensions;
+    this.location = location;
+    this.longitude = longitude;
+    this.latitude = latitude;
+    this.infoUrls = infoUrls;
+    this.pictureUrl = pictureUrl;
+  }
+}
+
+export default Artwork;
