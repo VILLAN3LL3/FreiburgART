77473a618d986262a789fb8e1142b95eef6fa378
Mira Manger <mira.manger@haufe-lexware.com>
2021-06-19 07:59:19 +0200

    use custom marker images

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Haufe Hackathon Freiburg 2021
 https://hackathon-freiburg.de/
+
+* We use icons from [icons8](https://icons8.com):
+- https://icons8.com/icon/3931/walking
+- https://icons8.com/icons/set/sculpture
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Container } from "react-native";
+import { StyleSheet, Text, View, Container } from 'react-native';
 import { ARTWORK_LIST } from '../data/dummy-data';
 import MapView, { Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 
-
-// https://github.com/react-native-maps/react-native-mapss
+// https://github.com/react-native-maps/react-native-maps
 // https://dev-yakuza.posstree.com/en/react-native/react-native-maps/
 const MapScreen = (props) => {
   const artworks = ARTWORK_LIST;
@@ -15,42 +14,48 @@ const MapScreen = (props) => {
     region: {
       latitude: 47.9959,
       longitude: 7.85222,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0121,
     },
   });
 
-onRegionChange = (region) => {
-  setRegion({ region });
-}
+  // onRegionChange = (region) => {
+  //   setRegion({ region });
+  // };
 
-// ToDo: Herausfinden, was man tun muss, damit Geolocation im Emulator funktioniert
-    // useEffect(() => {
-    //   Geolocation.getCurrentPosition(
-    //     (position) => {
-    //       const { latitude, longitude } = position.coords;
-    //       setLocation({
-    //         latitude,
-    //         longitude,
-    //       });
-    //     },
-    //     (error) => {
-    //       console.log(error.code, error.message);
-    //     },
-    //     { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    //   );
-    // }, []);
+  // ToDo: Herausfinden, was man tun muss, damit Geolocation im Emulator funktioniert
+  // useEffect(() => {
+  //   Geolocation.getCurrentPosition(
+  //     (position) => {
+  //       const { latitude, longitude } = position.coords;
+  //       setLocation({
+  //         latitude,
+  //         longitude,
+  //       });
+  //     },
+  //     (error) => {
+  //       console.log(error.code, error.message);
+  //     },
+  //     { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  //   );
+  // }, []);
 
   return (
     <View style={{ ...styles.container, ...props.style }}>
       {region && (
-        <MapView region={region.region} onRegionChange={onRegionChange} style={{ flex: 1 }}>
+        <MapView region={region.region} style={{ flex: 1 }}>
+          <Marker
+            key={0}
+            coordinate={{ latitude: region.region.latitude, longitude: region.region.longitude }}
+            image={require('../assets/walking-96.png')}
+          />
           {artworks.map((artwork) => (
             <Marker
               key={artwork.id}
               coordinate={{ latitude: artwork.latitude, longitude: artwork.longitude }}
               title={artwork.title}
               description={`${artwork.artists.join(', ')} (${artwork.year})`}
+              image={require('../assets/easel-96.png')}
             />
           ))}
         </MapView>
@@ -61,7 +66,7 @@ onRegionChange = (region) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 });
 
